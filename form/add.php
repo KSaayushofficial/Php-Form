aae7a9fa073a650313d2500d78103431ea96b1ba
KSaayushofficial <ksaayushofficial3@gmail.com>
2025-02-12 21:22:19 +0545

    Add the records into database

---
+++ form/add.php
@@ -0,0 +1,51 @@
+<?php 
+$conn = mysqli_connect('localhost','root','','bca');
+
+if (!$conn){
+    die("Database not connected.".mysqli_connect_error());
+}
+
+if(!empty($_POST)){
+
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $sql = "INSERT INTO students(Id,Name,Email,Address) VALUES ('$id','$name','$email','$address')";
+    $result = mysqli_query($conn,$sql);
+
+    if($result){
+        echo "Record added successfully.";
+     } else{
+            echo "Record not added.";
+        }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <blockquote>
+        <h1>Student Record</h1>
+        <a href="index.php">Home</a>
+        <a href="add.php">Add Student</a>
+        <hr>
+        <form action="" method="post">
+            <label for="id">Id</label> <br>
+            <input type="text" name="id"> <br>
+            <label for="name">Name</label> <br>
+            <input type="text" name="name"> <br>
+            <label for="email">Email</label> <br>
+            <input type="email" name="email"> <br>
+            <label for="address">Address</label> <br>
+            <input type="text" name="address"> <br>
+            <button>Add Record</button>
+        </form>
+    </blockquote>
+</body>
+</html>
